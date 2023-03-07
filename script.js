bd80ac26dbcea8c1dfd1885871631bc1be1a0c1a
G-Lumia <giorgiolumia@gmail.com>
2023-03-07 16:09:43 +0100

    added an else option

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ const userName = prompt('Enter your name');
 const userSurname = prompt('Enter your surname');
 const userFavoriteColor = prompt('Enter your favorite color');
 
-document.writeln('Your secret password is: ' + userName + userSurname + userFavoriteColor + 21);
+
+if(isNaN(userName) && isNaN(userSurname) && isNaN(userFavoriteColor)){
+    document.writeln('Your secret password is: ' + userName + userSurname + userFavoriteColor + 21);
+}
+
+else{
+    document.writeln('Incorrect values. Sorry, reload the page and try again')
+}
