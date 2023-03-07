d34d56b372ef311135901277a80752882c40d71c
G-Lumia <giorgiolumia@gmail.com>
2023-03-07 14:29:53 +0100

    project closed

---
+++ script.js
@@ -0,0 +1,5 @@
+const userName = prompt('Enter your name');
+const userSurname = prompt('Enter your surname');
+const userFavoriteColor = prompt('Enter your favorite color');
+
+document.writeln('Your secret password is: ' + userName + userSurname + userFavoriteColor + 21);
